0dce27a35789eb24e8b8df47447aece23c411af2
stuckinadessert <stuckinadessert@gmail.com>
2017-10-04 11:09:15 -0700

    Upload of existing art assets
    Current assets as of 10/4/2017

---
+++ Art/CSVania.tsx
@@ -0,0 +1,100 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="CSVania" tilewidth="32" tileheight="32" tilecount="32" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="Tiles/blue bricks.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="Tiles/blue bricks top.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="Tiles/blue bricks RTCorner.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="Tiles/blue bricks Rwall.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="Tiles/blue bricks Lwall.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="Tiles/blue bricks top left corner.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="Tiles/blue bricks bottom.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="Tiles/blue bricks LBcorner.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="Tiles/blue bricks RBcorner.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="Tiles/blue bricks BLcorner.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="Tiles/blue bricks BRcorner.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="Tiles/spike pit bloody.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="Tiles/spike pit clean.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="Tiles/spike pit Lwall.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="Tiles/spike pit Rwall.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="Tiles/Dark Dirt.png"/>
+ </tile>
+ <tile id="18">
+  <image width="32" height="32" source="Tiles/dark dirt T uncovered.png"/>
+ </tile>
+ <tile id="20">
+  <image width="32" height="32" source="Tiles/Dark Dirt T covered.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="32" source="Tiles/blue bricksbackground.png"/>
+ </tile>
+ <tile id="22">
+  <image width="32" height="32" source="Tiles/Dark Dirt Wall L.png"/>
+ </tile>
+ <tile id="23">
+  <image width="32" height="32" source="Tiles/Dark Dirt Wall R.png"/>
+ </tile>
+ <tile id="24">
+  <image width="32" height="32" source="Tiles/ddTLC.png"/>
+ </tile>
+ <tile id="25">
+  <image width="32" height="32" source="Tiles/ddTRC.png"/>
+ </tile>
+ <tile id="26">
+  <image width="32" height="32" source="Tiles/ddBRC.png"/>
+ </tile>
+ <tile id="27">
+  <image width="32" height="32" source="Tiles/ddBLC.png"/>
+ </tile>
+ <tile id="28">
+  <image width="32" height="32" source="Tiles/ddB.png"/>
+ </tile>
+ <tile id="29">
+  <image width="32" height="32" source="Tiles/ddBRSC.png"/>
+ </tile>
+ <tile id="30">
+  <image width="32" height="32" source="Tiles/ddBRIC.png"/>
+ </tile>
+ <tile id="31">
+  <image width="32" height="32" source="Tiles/bbBRIC.png"/>
+ </tile>
+ <tile id="32">
+  <image width="32" height="32" source="Tiles/bbBLIC.png"/>
+ </tile>
+ <tile id="33">
+  <image width="32" height="32" source="Tiles/ddBLSC.png"/>
+ </tile>
+ <tile id="34">
+  <image width="32" height="32" source="Tiles/ddBLIC.png"/>
+ </tile>
+</tileset>
