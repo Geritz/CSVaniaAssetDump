428caa43c68af783c0781035a483097df3bbc043
senatoj <senatoj@oregonstate.edu>
2017-10-06 08:36:42 -0500

    Add files via upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public UIManager UI;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void TogglePauseMenu() 
+    {
+        if (UI.GetComponentInChildren<Canvas>().enabled)
+        {
+            UI.GetComponentInChildren<Canvas>().enabled = false;
+            Time.timeScale = 1.0f;
+        }
+        else
+        {
+            UI.GetComponentInChildren<Canvas>().enabled = true;
+            Time.timeScale = 0f;
+        }
+
+        Debug.Log("GAMEMANAGER: TimeScale: " + Time.timeScale);
+    }
+}
